e8ec4e3ae5fa2d4fa2257a2346901e500b891155
Meraiku <meraikuu@gmail.com>
2024-11-17 05:39:40 +0300

    Add attr aliases for logging wrapper

--- pkg/logging/alias.go
+++ pkg/logging/alias.go
@@ -22,4 +22,13 @@ var (
 	NewJSONHandler = slog.NewJSONHandler
 	Default        = slog.Default
 	SetDefault     = slog.SetDefault
+
+	StringAttr   = slog.String
+	BoolAttr     = slog.Bool
+	Float64Attr  = slog.Float64
+	AnyAttr      = slog.Any
+	DurationAttr = slog.Duration
+	IntAttr      = slog.Int
+	Int64Attr    = slog.Int64
+	Uint64Attr   = slog.Uint64
 )
